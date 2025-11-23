95f247f13c9705fe08569fe185972f3f1ca6fe2d
Ryven <matilevy858@gmail.com>
2025-11-23 13:02:07 +0100

    asd

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var Strona_glowna = document.getElementById("Strona_glowna");
 var O_nas = document.getElementById("O_nas");
+var body = document.getElementById("body"); 
+
+
 
 var menuLabels = document.querySelectorAll("#menu label");
 var aktualna_zakladka = Strona_glowna;
+
+body.style.animation = "otwieranie_zakladki 1s ease-in-out forwards";
 
 function clearActiveLabels() {
     menuLabels.forEach(label => label.classList.remove("active-menu"));
@@ -16,8 +21,10 @@ function setActiveLabel(zakladka) {
         menuLabels[1].classList.add("active-menu");
     } else if (zakladka === Nasze_uslugi) {
         menuLabels[2].classList.add("active-menu");
-    } else if (zakladka === Kontakt) {
+    } else if (zakladka === Dlaczego_my) {
         menuLabels[3].classList.add("active-menu");
+    } else if (zakladka === Kontakt) {
+        menuLabels[4].classList.add("active-menu");
     }
     // Add more else if for other tabs if needed
 }
